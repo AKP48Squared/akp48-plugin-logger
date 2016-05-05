b166466dbfc152864ab19e54fd2d7c475384322a
Austin Peterson <austin@akpwebdesign.com>
2016-05-05 09:31:32 -0500

    GLOBAL -> global

--- main.js
+++ main.js
@@ -17,14 +17,14 @@ Logger.prototype.handleMessage = function (message, context, res) {
 
 Logger.prototype.handleFullMessage = function (message, context) {
   var out = `<=== ${context.instanceId}:${context.to} | ${context.nick} | ${c.stripColorsAndStyle(message)}`;
-  GLOBAL.logger.stupid(out);
+  global.logger.stupid(out);
 };
 
 Logger.prototype.handleSentMessage = function (to, message, context) {
   var xtra = '';
   if(context.isEmote) {xtra = '/me ';}
   var out = `===> ${context.instanceId}:${to} | ${(context.myNick ? context.myNick + ' | ' : '')}${xtra}${c.stripColorsAndStyle(message)}`;
-  setTimeout(function(){GLOBAL.logger.stupid(out);},1); // Send to console after one millisecond,
+  setTimeout(function(){global.logger.stupid(out);},1); // Send to console after one millisecond,
   // because for some reason, sent messages are being handled faster than received messages.
   // This may be because of how the event listeners are working, but I'm not sure.
 };
